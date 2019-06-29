9b3f7fa45065d73fe0812c681a649d26b637bf17
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-06-28 20:48:21 -0400

    pt9 updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,44 @@
-var $ = function (id) {
-    return document.getElementById(id);
-}
-
 var words = ["moon", "star", "mars", "venus", "jupiter", "mercury", "artemis", "tuxedo", "luna", "crystal", "power", "love", "sailor"];
-var choice = Math.floor(Math.random() * words.length)
-var answer = words[choice];
-var myLength = answer.length;
-var display=[myLength];
-var win = myLength;
-var letters = answer.split('');
-var attemptsLeft= 10;
-var output="";
-var userLetter="";
 
-var setup = function()
-{
-    for (var i=0; i< answer.length; i++)
-    {
-        display[i] = "- ";
-        output = output + display[i];
-    }
-    document.getElementById("words").innerHTML = output;
-    output ="";
-}
+// var $ = function (id) {
+//     return document.getElementById(id);
+// }
+// var choice = Math.floor(Math.random() * words.length)
+// var answer = words[choice];
+// var myLength = answer.length;
+// var display=[myLength];
+// var win = myLength;
+// var letters = answer.split('');
+// var attemptsLeft= 10;
+// var output="";
+// var userLetter="";
 
-var submit = function()
-{
-    output = "";
-    userLetter=$("letter").value;
-    $("letter").value = "";
+// var setup = function()
+// {
+//     for (var i=0; i< answer.length; i++)
+//     {
+//         display[i] = "- ";
+//         output = output + display[i];
+//     }
+//     document.getElementById("words").innerHTML = output;
+//     output ="";
+// }
 
-    for (var x=0; x<answer.length; c++)
-    {
-        alert(letters[x]);
-        if (userLetter.toLowerCase() == letters[x])
-        {
-            display[x] = userLetter.toLowerCase();
-            win--;
-        }
-        output = output + display[x] + " ";
-    }
-    
-}
+// var submit = function()
+// {
+//     output = "";
+//     userLetter=$("letter").value;
+//     $("letter").value = "";
+
+//     for (var x=0; x<answer.length; c++)
+//     {
+//         alert(letters[x]);
+//         if (userLetter.toLowerCase() == letters[x])
+//         {
+//             display[x] = userLetter.toLowerCase();
+//             win--;
+//         }
+//         output = output + display[x] + " ";
+//     }
+
+// }
