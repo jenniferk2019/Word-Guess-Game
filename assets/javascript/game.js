329d288a1a0c0879ea87ce827ef698d24370d620
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-06-28 23:53:36 -0400

    pt13 updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //Array
-var words = ["moon", "mars", "venus", "jupiter", "mercury", "artemis", "luna", "crystal"] 
+var words = ["moon", "mars", "venus", "jupiter", "mercury", "artemis", "luna", "crystal"]
 
 //Empty Variables 
 var randomWord = "";
@@ -22,12 +22,78 @@ function Game() {
 
     for (var i = 0; i < blanks; i++) {
         blanksAndCorrect.push("_");
+    }
+
+    document.getElementById("currentword").innerHTML = " " + blanksAndCorrect.join(" ");
+
 }
 
-document.getElementById("currentword").innerHTML = " " + blanksAndCorrect.join(" ");
+//pic functions
+var moon = document.getElementById("moon");
+var mars = document.getElementById("mars");
+var venus = document.getElementById("venus");
+var jupiter = document.getElementById("jupiter");
+var mercury = document.getElementById("mercury");
+var artemis = document.getElementById("artemis");
+var luna = document.getElementById("luna");
+var crystal = document.getElementById("crystal");
 
-console.log(randomWord);
-console.log(lettersOfWord);
-console.log(blanks);
-console.log(blanksAndCorrect);
+function pic() {
+    if (randomWord === words[0]) {
+        document.getElementById("image").src = "./assets/images/moon.jpg";
+    }
+    else if (randomWord === words[1]) {
+        document.getElementById("image").src = "./assets/images/mars.jpg";
+    }
+    else if (randomWord === words[2]) {
+        document.getElementById("image").src = "./assets/images/venus.jpg";
+    }
+    else if (randomWord === words[3]) {
+        document.getElementById("image").src = "./assets/images/jupiter.jpg";
+    }
+    else if (randomWord === words[4]) {
+        document.getElementById("image").src = "./assets/images/mercury.jpg";
+    }
+    else if (randomWord === words[5]) {
+        document.getElementById("image").src = "./assets/images/artemis.jpg";
+    }
+    else if (randomWord === words[6]) {
+        document.getElementById("image").src = "./assets/images/luna.jpg";
+    }
+    else if (randomWord === words[7]) {
+        document.getElementById("image").src = "./assets/images/crystal.jpg";
+    }
+};
+
+//Reset Game Function
+function reset() {
+    guessesRemaining = 10;
+    blanksAndCorrect = []
+    wrongGuess = []
+    Game ()
 }
+
+// [if/else] letter chosen matches random word
+function checkLetters(letter) {
+    var lettersOfWord = false;
+    for (var i = 0; i < blanks; i++) {
+        if (randomWord[i] == letter) {
+            lettersOfWord = true;
+        }
+    }
+
+// [if] letter false
+if (lettersOfWord) {
+    for (var i = 0; i < blanks; i++) {
+        if (randomWord[i] == letter) {
+            blanksAndCorrect[i] = letter;
+        }
+    }
+}
+
+//otherwise, wrong guesses reduce remaining guesses
+else {
+    wrongGuess.push(letter);
+    guessesRemaining--;
+}
+};
