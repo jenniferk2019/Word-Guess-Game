2e551a66316e6914f1a16be301febe6654d969dd
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-06-29 15:43:20 -0400

    pt18 updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,8 +21,39 @@ var remainingLetters = word.length;
 
 //*************** MAIN GAME *******************************//
 
-//Guesses left to be guessed_Main loop
-while (remainingLetters > 0)
+//Letter/word guesses left to be guessed_Main loop
+while (remainingLetters > 0) {
+//Show player progress
+document.getElementById("wordblank").innerHTML = anserArray.join("");
+//Get a guess from player
+var guess = prompt("Guess a letter!");
+//If left blank
+if (guess === null) {
+    //exit game
+    break;
+//If guess is more than one letter or no letters
+}else if (guess.length !== 1) {
+    //alert to guess single letter
+    alert("Enter a single letter");
+//If correct guess update game state with player guess
+}else{
+    for (var j = 0; j < word.length; j++ ) { 
+//If the letter guessed is in word at this point
+if (word[j] === guess) {
+//Update answerArray with letter guessed at this point
+    answerArray[j] = guess;
+//Then subject one form remaining letters
+remainingLetters--;
+}
+}
+}
+//***********END OF MAIN GAME LOOP *******************/
+}
+//Let player know word
+alert(answerArray.join(" "));
+//Congratulate the player
+alert("Good job Sailor Scout!" + word);
+
 
 // //Empty Variables 
 // var word = "";
