784b4d3f4f7664f99f437989e8c42a84990d74a5
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-06-29 23:04:23 -0400

    FINAL Homework3 updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,136 +1,112 @@
 //GAME SETUP--------------------------->
-(function () {
-    "use strict";
-    var availableLetters, words, guessInput, guess, guessButton, lettersGuessed, lettersMatched, output, sailor, letters, lives, currentWord, numLettersMatched, messages;
+$(document).ready(function () {
 
-function setup() {
-    availableLetters = "abcdefghijklmnopqrstuvwxyz";
-    lives = 5;
-    words = ["moon", "mars", "venus", "jupiter", "mercury", "artemis", "luna", "crystal"];
-    messages = {
-        win: 'Congrats Sailor Scout!',
-        lose: 'You Lost'
-        guessed: 'Already used letter. Pick another.'
-        validLetter: 'Enter a letter a-z'
+    //Array
+    var words = ["moon", "mars", "venus", "jupiter", "mercury", "artemis", "luna", "crystal"]
+ 
+    //# of max guesses
+    const maxGuess = 10
+ 
+    //Variables
+    var guessedLetters = []
+    var guessingWord = []
+    var wordMatch
+    var numGuess
+    var wins = 0
+    var pauseGame = false
+ 
+    resetGame()
+ 
+    //Keypress function
+    document.onkeypress = function (event) {
+ 
+        // Make sure key pressed is lower case and a letter
+        if (isAlpha(event.key) && !pauseGame) {
+            checkForLetter(event.key.toLowerCase())
+        }
+    }
+ 
+    //Game functions - check the letter and make sure it's correct
+    function checkForLetter(letter) {
+        var foundLetter = false
+        var correctPic = document.createElement("pic")
+        var incorrectPic = document.createElement("pic")
+        correctPic.setAttribute("src", "assets/images/win.jpg")
+        incorrectPic.setAttribute("src", "assets/images/lose.jpg");
+ 
+        // Search wordsArray for letter
+        for (var i = 0, j = wordMatch.length; i < j; i++) {
+            if (letter === wordMatch[i]) {
+                guessingWord[i] = letter
+                foundLetter = true
+                correctPic.show()
+ 
+                // If guessing word matches random word
+                if (guessingWord.join("") === wordMatch) {
+ 
+                    //# of wins
+                    wins++
+                    pauseGame = true
+                    updateDisplay()
+                    setTimeout(resetGame, 5000)
+                }
+            }
+        }
+ 
+        if (!foundLetter) {
+            incorrectPic.show()
+ 
+            // Check if inccorrect guess is on the list
+            if (!guessedLetters.includes(letter)) {
+ 
+                // Add incorrect letter to guessed list
+                guessedLetters.push(letter)
+ 
+                // Lower the number of remaining guesses
+                numGuess--
+            }
+            if (numGuess === 0) {
+ 
+                // Show the word before starting the game again
+                guessingWord = wordToMatch.split()
+                pauseGame = true
+                setTimeout(resetGame, 5000)
+            }
+        }
+ 
+        updateDisplay()
+ 
+    }
+ 
+    // Check in keypressed is between [Alpha] a-z
+    function isAlpha(ch) {
+        return /^[A-Z]$/i.test(ch);
+    }
+ 
+    function resetGame() {
+        numGuess = maxGuess
+        pauseGame = false
+ 
+        // Get new word
+        wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].tolowerCase()
+ 
+        console.log(wordToMatch)
+ 
+        // Reset word arrays
+        guessedLetters = []
+        guessingWord = []
+ 
+        // Reset the guessed word
+        for (var i = 0, j = wordMatch.length; i < j; i++) {
+ 
+            // Update the Display
+            updateDisplay()
+        }
+ 
+        function updateDisplay() {
+            document.getElementById("totalWins").innerHTML = wins
+            document.getElementById("currentWord").innerText = guessingWord.join("")
+            document.getElementById("remainingGuesses").innerText = numGuess
+            document.getElementById("guessedLetters").innerText = guessedLetters.join(" ")
+        }
     };
-
-    letterGuessed = lettersMatched = '';
-    numLettersMatched = 0;
-
-    //Choose a word
-    currentWord = words[Math.floor(Math.random() * words.length)];
-
-    //Vars for later access
-    output = document.getElementById("output");
-    sailor = document.getElementById("sailor");
-    guessInput = document.getElementById("letter");
-
-    // //# of max guesses
-    // const maxGuess = 10
-
-    // //Variables
-    // var guessedLetters = []
-    // var guessingWord = []
-    // var wordMatch
-    // var numGuess
-    // var wins = 0
-    // var pauseGame = false
-    // var dashes = [_]
-    
-    // resetGame()
-
-    // //Keypress function
-    // document.onkeypress = function (event) {
-
-    //     // Make sure key pressed is lower case and a letter
-    //     if (isAlpha(event.key) && !pauseGame) {
-    //         checkForLetter(event.key.toLowerCase())
-    //     }
-    // }
-
-    // //Game functions - check the letter and make sure it's correct
-    // function checkForLetter(letter) {
-    //     var foundLetter = false
-    //     var correctPic = document.createElement("pic")
-    //     var incorrectPic = document.createElement("pic")
-    //     correctPic.setAttribute("src", "assets/images/win.jpg")
-    //     incorrectPic.setAttribute("src", "assets/images/lose.jpg");
-
-    //     // Search wordsArray for letter
-    //     for (var i = 0, j = wordMatch.length; i < j; i++) {
-    //         if (letter === wordMatch[i]) {
-    //             guessingWord[i] = letter
-    //             foundLetter = true
-    //             correctPic.show()
-
-    //             // If guessing word matches random word
-    //             if (guessingWord.join("") === wordMatch) {
-
-    //                 //# of wins
-    //                 wins++
-    //                 pauseGame = true
-    //                 updateDisplay()
-    //                 setTimeout(resetGame, 5000)
-    //             }
-    //         }
-    //     }
-
-    //     if (!foundLetter) {
-    //         incorrectPic.show()
-
-    //         // Check if inccorrect guess is on the list
-    //         if (!guessedLetters.includes(letter)) {
-
-    //             // Add incorrect letter to guessed list
-    //             guessedLetters.push(letter)
-
-    //             // Lower the number of remaining guesses
-    //             numGuess--
-    //         }
-    //         if (numGuess === 0) {
-
-    //             // Show the word before starting the game again
-    //             guessingWord = wordMatch.split()
-    //             pauseGame = true
-    //             setTimeout(resetGame, 5000)
-    //         }
-    //     }
-
-    //     updateDisplay()
-
-    // }
-
-    // // Check in keypressed is between [Alpha] a-z
-    // function isAlpha(ch) {
-    //     return [i].test(ch);
-    // }
-
-    // function resetGame()
-    // {
-    //     numGuess = maxGuess
-    //     pauseGame = false
-
-    //     // Get new word
-    //     wordMatch = words[Math.floor(Math.random() * words.length)]
-
-    //     // Reset word arrays
-    //     guessedLetters = []
-    //     guessingWord = []
-
-    //     // Reset the guessed word
-    //     for (var i = 0, j = wordMatch.length; i < j; i++) {
-
-    //         // Update the Display
-    //         updateDisplay()
-    //     }
-
-    // }
-
-    //     // function updateDisplay() {
-    //     //     document.getElementById("totalWins").HTMLElement = wins
-    //         currentword = document.getElementById("currentWord");
-    //         currentword.innerHTML = '<li class="current-word">Current word:
-    //         </li';
-    //         // document.getElementById("remainingGuesses").HTMLElement = numGuess
-    //         // document.getElementById("guessedLetters").HTMLElement = guessedLetters.join(" ")
-    //     }
