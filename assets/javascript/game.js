495aacb373a256ab2dbfeb4cd6858d61c088dcf6
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-07-03 14:43:59 -0400

    added more pseudo code and made final edits to javascript portion

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,99 +6,55 @@
     //# of max guesses
     const maxGuess = 10
  
-    //Variables
+    // Empty variables
     var guessedLetters = []
     var guessingWord = []
-    var wordMatch
-    var numGuess
+    var wordMatch = 0;
+    var numGuess = 0;
     var wins = 0;
-    var pauseGame = false
  
-    resetGame()
+    // resetGame() ?Do I reset here??
  
     //Keypress function - when the user hits a letter it adds to the blank word
     document.onkeypress = function (event) {
  
-        // Make sure key pressed is lower case and a letter
-        if (isAlpha(event.key) && !pauseGame) {
-            checkForLetter(event.key.toLowerCase())
-        }
-    }
- 
-    //Game functions - check the letter and make sure it's correct
-    function checkForLetter(letter) {
-        var foundLetter = false
-        var correctPic = document.createElement("pic")
-        var incorrectPic = document.createElement("pic")
+    //Game functions - check the letter  to see if matched in the word in the array and make sure it's correct then
+    
+    //Populate images to let user know they either guessed the word correct or got it wrong.
+
         correctPic.setAttribute("src", "assets/images/win.jpg")
         incorrectPic.setAttribute("src", "assets/images/lose.jpg");
  
-        // Search wordsArray for letter
+        // Using for loop to search wordsArray for letter
         for (var i = 0, j = wordMatch.length; i < j; i++) {
             if (letter === wordMatch[i]) {
-                guessingWord[i] = letter
-                foundLetter = true
-                correctPic.show()
  
-                // If guessing word matches random word
+        // If guessing word matches random word
                 if (guessingWord.join("") === wordMatch) {
  
-                    //# of wins
-                    wins++
-                    pauseGame = true
-                    updateDisplay()
-                    setTimeout(resetGame, 5000)
-                }
-            }
-        }
+        // Populate # of wins & losses
  
-        if (!foundLetter) {
-            incorrectPic.show()
+        // Check if inccorrect guess is on the list
+            if () {
+
+                else ()
+
+                else if ()
  
-            // Check if inccorrect guess is on the list
-            if (!guessedLetters.includes(letter)) {
+        // Add incorrect letter to guessed list
  
-                // Add incorrect letter to guessed list
-                guessedLetters.push(letter)
+        // Lower the number of remaining guesses
  
-                // Lower the number of remaining guesses
-                numGuess--
-            }
-            if (numGuess === 0) {
- 
-                // Show the word before starting the game again
-                guessingWord = wordToMatch.split()
-                pauseGame = true
-                setTimeout(resetGame, 5000)
-            }
-        }
- 
-        updateDisplay()
- 
-    }
- 
-    // Check in keypressed is between [Alpha] a-z
-   // function isAlpha(ch) {
-    //    return /^[A-Z]$/i.test(ch);
-   // }
- 
-    function resetGame() {
-        numGuess = maxGuess
-        pauseGame = false
+        // Show the word before starting the game again
  
         // Get new word
         wordMatch = words[Math.floor(Math.random() * words.length)];
  
-        console.log(wordMatch)
- 
         // Reset word arrays
-        guessedLetters = []
-        guessingWord = []
- 
-        // Reset the guessed word
-        for (var i = 0, j = wordMatch.length; i < j; i++) {
- 
-            // Update the Display
+       
+    // Reset () ??Do I reset game here??
+
+    // Update the Display
         }
  
         function updateDisplay() {
@@ -108,5 +64,5 @@
             document.getElementById("guessedLetters").innerText = guessedLetters.join(" ")
         } updateDisplay()
 
-// end game??? or reset to play again
-    };
+        // Reset game here ??
+    )};
